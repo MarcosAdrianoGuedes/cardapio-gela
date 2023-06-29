01177579e8ca75f9eedd80b27f8a509a7a0db254
MarcosAdrianoGuedes <marcosadri55@hotmail.com>
2023-06-29 14:11:01 -0300

    cardápio gela guela

--- src/App.js
+++ src/App.js
@@ -41,10 +41,10 @@ function App() {
       </div>
       <div className="buttons-container">
         <button className="section-button" onClick={() => scrollToSection('cervejas')}>Cervejas</button>
-        <button className="section-button" onClick={() => scrollToSection('petiscos')}>Petiscos</button>
+        <button className="section-button" onClick={() => scrollToSection('petiscos')}>Porções</button>
         <button className="section-button" onClick={() => scrollToSection('doses')}>Doses</button>
         <button className="section-button" onClick={() => scrollToSection('drinks')}>Drinks</button>
-        <button className="section-button" onClick={() => scrollToSection('tabacos')}>Tabacos/Cigarros</button>
+        <button className="section-button" onClick={() => scrollToSection('tabacos')}>Latas Bebidas/Águas</button>
       </div>
       <div className="menu-container" onclick="toggleZoom(this)">
         <div className="container" id="cervejas">
@@ -54,49 +54,65 @@ function App() {
               <h3 className="subsection-title">LITRÕES</h3>
               <ul>
                 <li>
+                  <span className="item-name">Antarctica</span>
+                  <span className="item-price">R$12,00</span>
+                </li>
+                <li>
                   <span className="item-name">Brahma</span>
-                  <span className="item-price">R$9,00</span>
+                  <span className="item-price">R$12,00</span>
                 </li>
                 <li>
-                  <span className="item-name">Original</span>
-                  <span className="item-price">R$10,00</span>
-                </li>
-                <li>
-                  <span className="item-name">Heineken</span>
-                  <span className="item-price">R$13,00</span>
-                </li>
-                <li>
-                  <span className="item-name">Skol</span>
-                  <span className="item-price">R$8,00</span>
-                </li>
-                <li>
-                  <span className="item-name">Antártica</span>
-                  <span className="item-price">R$10,00</span>
-                </li>
-                <li>
-                  <span className="item-name">Itaipava</span>
-                  <span className="item-price">R$7,00</span>
-                </li>
-                <li>
-                  <span className="item-name">Budwaiser</span>
-                  <span className="item-price">R$11,00</span>
+                  <span className="item-name">Budweiser</span>
+                  <span className="item-price">R$12,00</span>
                 </li>
               </ul>
             </div>
             <div className="subsection">
-              <h3 className="subsection-title">LATINHAS</h3>
+              <h3 className="subsection-title">LATAS</h3>
               <ul>
                 <li>
-                  <span className="item-name">Cerveja 1</span>
-                  <span className="item-price">R$9,00</span>
+                  <span className="item-name">Antactica</span>
+                  <span className="item-price">R$4,00</span>
                 </li>
                 <li>
-                  <span className="item-name">Cerveja 2</span>
-                  <span className="item-price">R$10,00</span>
+                  <span className="item-name">Devassa</span>
+                  <span className="item-price">R$4,00</span>
                 </li>
                 <li>
-                  <span className="item-name">Cerveja 3</span>
-                  <span className="item-price">R$13,00</span>
+                  <span className="item-name">Skol</span>
+                  <span className="item-price">R$4,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Amstel</span>
+                  <span className="item-price">R$4,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Brahma</span>
+                  <span className="item-price">R$4,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Imperio</span>
+                  <span className="item-price">R$3,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Bohemia</span>
+                  <span className="item-price">R$5,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Duplo Malte</span>
+                  <span className="item-price">R$5,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Eisenbahn</span>
+                  <span className="item-price">R$5,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Original</span>
+                  <span className="item-price">R$4,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Spaten</span>
+                  <span className="item-price">R$5,00</span>
                 </li>
               </ul>
             </div>
@@ -104,35 +120,136 @@ function App() {
               <h3 className="subsection-title">LONGNECK</h3>
               <ul>
                 <li>
-                  <span className="item-name">Cerveja 4</span>
+                  <span className="item-name">Heineken</span>
+                  <span className="item-price">R$8,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Heineken 0 Álcool</span>
                   <span className="item-price">R$9,00</span>
                 </li>
                 <li>
-                  <span className="item-name">Cerveja 5</span>
+                  <span className="item-name">Brahma 0 Álcool</span>
+                  <span className="item-price">R$6,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Bohemia</span>
+                  <span className="item-price">R$5,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Spaten</span>
+                  <span className="item-price">R$8,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Stella Artois</span>
+                  <span className="item-price">R$8,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Sol</span>
+                  <span className="item-price">R$7,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Malzebier</span>
+                  <span className="item-price">R$7,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Caruru</span>
+                  <span className="item-price">R$7,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Budweiser</span>
+                  <span className="item-price">R$8,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Corona</span>
+                  <span className="item-price">R$6,00</span>
+                </li>
+              </ul>
+            </div>
+            <div className="subsection">
+              <h3 className="subsection-title">CERVEJAS 600ML</h3>
+              <ul>
+                <li>
+                  <span className="item-name">Antarctica</span>
+                  <span className="item-price">R$9,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Devassa</span>
+                  <span className="item-price">R$9,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Skol</span>
+                  <span className="item-price">R$9,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Amstel</span>
+                  <span className="item-price">R$9,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Brahma</span>
+                  <span className="item-price">R$9,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Bohemia</span>
+                  <span className="item-price">R$9,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Duplo Malte</span>
+                  <span className="item-price">R$9,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Eisenbahn</span>
                   <span className="item-price">R$10,00</span>
                 </li>
                 <li>
-                  <span className="item-name">Cerveja 6</span>
-                  <span className="item-price">R$13,00</span>
+                  <span className="item-name">Original</span>
+                  <span className="item-price">R$10,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Heineken</span>
+                  <span className="item-price">R$14,00</span>
+                </li>
+                <li>
+                  <span className="item-name">Spaten</span>
+                  <span className="item-price">R$12,00</span>
                 </li>
               </ul>
             </div>
           </div>
         </div>
         <div className="container" id="petiscos">
-          <h2 className="section-title">PETISCOS</h2>
+          <h2 className="section-title">PORÇÕES</h2>
           <ul>
             <li>
-              <span className="item-name">Batata Frita</span>
-              <span className="item-price">R$10,00</span>
+              <span className="item-name">Anel de Cebola</span>
+              <span className="item-price">R$25,00</span>
             </li>
             <li>
-              <span className="item-name">Churrasquinho</span>
-              <span className="item-price">R$10,00</span>
+              <span className="item-name">Calabresa</span>
+              <span className="item-price">R$30,00</span>
             </li>
             <li>
-              <span className="item-name">Frango Frito</span>
-              <span className="item-price">R$10,00</span>
+              <span className="item-name">Coxinha</span>
+              <span className="item-price">R$30,00</span>
+            </li>
+            <li>
+              <span className="item-name">Coxinha</span>
+              <span className="item-price">R$30,00</span>
+            </li>
+            <li>
+              <span className="item-name">Isca de Peixe</span>
+              <span className="item-price">R$45,00</span>
+            </li>
+            <li>
+              <span className="item-name">Batata Frita com Calabresa(Cheddar)</span>
+              <span className="item-price">R$30,00</span>
+            </li>
+            <li>
+              <span className="item-name">Batata Frita Comum</span>
+              <span className="item-price">R$25,00</span>
+            </li>
+            <li>
+              <span className="item-name">Carne de Sol</span>
+              <span className="item-price">R$45,00</span>
             </li>
           </ul>
         </div>
@@ -140,16 +257,44 @@ function App() {
           <h2 className="section-title">DOSES</h2>
           <ul>
             <li>
-              <span className="item-name">Dose 1</span>
+              <span className="item-name">Chivas</span>
+              <span className="item-price">R$20,00</span>
+            </li>
+            <li>
+              <span className="item-name">Tequila</span>
+              <span className="item-price">R$13,00</span>
+            </li>
+            <li>
+              <span className="item-name">Old Par</span>
+              <span className="item-price">R$25,00</span>
+            </li>
+            <li>
+              <span className="item-name">Amarula</span>
               <span className="item-price">R$10,00</span>
             </li>
             <li>
-              <span className="item-name">Dose 2</span>
-              <span className="item-price">R$10,00</span>
+              <span className="item-name">Vodka</span>
+              <span className="item-price">R$5,00</span>
             </li>
             <li>
-              <span className="item-name">Dose 3</span>
-              <span className="item-price">R$10,00</span>
+              <span className="item-name">Gin</span>
+              <span className="item-price">R$5,00</span>
+            </li>
+            <li>
+              <span className="item-name">São Francisco</span>
+              <span className="item-price">R$5,00</span>
+            </li>
+            <li>
+              <span className="item-name">Cachaça Temperada</span>
+              <span className="item-price">R$4,00</span>
+            </li>
+            <li>
+              <span className="item-name">Paratudo</span>
+              <span className="item-price">R$4,00</span>
+            </li>
+            <li>
+              <span className="item-name">Demais Doses</span>
+              <span className="item-price">R$4,00</span>
             </li>
           </ul>
         </div>
@@ -157,35 +302,124 @@ function App() {
           <h2 className="section-title">DRINKS</h2>
           <ul>
             <li>
-              <span className="item-name">Drink 1</span>
+              <span className="item-name">Caipirinha (51)</span>
               <span className="item-price">R$10,00</span>
             </li>
             <li>
-              <span className="item-name">Drink 2</span>
-              <span className="item-price">R$10,00</span>
+              <span className="item-name">Caipivodka (Sminoff)</span>
+              <span className="item-price">R$15,00</span>
             </li>
             <li>
-              <span className="item-name">Drink 3</span>
-              <span className="item-price">R$10,00</span>
+              <span className="item-name">Cozumel Taça</span>
+              <span className="item-price">R$15,00</span>
+            </li>
+            <li>
+              <span className="item-name">Preparo Cozumel Copo</span>
+              <span className="item-price">R$3,00</span>
+            </li>
+            <li>
+              <span className="item-name">Preparo Cozumel Taça</span>
+              <span className="item-price">R$4,00</span>
+            </li>
+            <li>
+              <span className="item-name">Tônica Tanqueray 600ML</span>
+              <span className="item-price">R$25,00</span>
+            </li>
+            <li>
+              <span className="item-name">Tõnica Gin Rock 600ML</span>
+              <span className="item-price">R$25,00</span>
+            </li>
+            <li>
+              <span className="item-name">Gin Red Bull Sabores</span>
+              <span className="item-price">R$20,00</span>
+            </li>
+            <li>
+              <span className="item-name">Cuba Libre</span>
+              <span className="item-price">R$12,00</span>
+            </li>
+            <li>
+              <span className="item-name">Drink Tentação</span>
+              <span className="item-price">R$20,00</span>
             </li>
           </ul>
         </div>
         <div className="container" id="tabacos">
-          <h2 className="section-title">TABACOS/CIGARROS</h2>
+          <h2 className="section-title">LATAS BEBIDAS/ÁGUAS</h2>
           <ul>
             <li>
-              <span className="item-name">Tabaco 1</span>
+              <span className="item-name">Refrigerante Lata 290ML</span>
+              <span className="item-price">R$4,00</span>
+            </li>
+            <li>
+              <span className="item-name">Refrigerante Lata 310ML</span>
+              <span className="item-price">R$5,00</span>
+            </li>
+            <li>
+              <span className="item-name">Coca KS</span>
+              <span className="item-price">R$4,50</span>
+            </li>
+            <li>
+              <span className="item-name">Monster Sabores</span>
               <span className="item-price">R$10,00</span>
             </li>
             <li>
-              <span className="item-name">Tabaco 2</span>
+              <span className="item-name">Suco Del Valle Sabores</span>
               <span className="item-price">R$10,00</span>
             </li>
             <li>
-              <span className="item-name">Tabaco 3</span>
+              <span className="item-name">51 ICE</span>
+              <span className="item-price">R$8,00</span>
+            </li>
+            <li>
+              <span className="item-name">Smirnoff ICE</span>
+              <span className="item-price">R$10,00</span>
+            </li>
+            <li>
+              <span className="item-name">Keep Cooler</span>
+              <span className="item-price">R$10,00</span>
+            </li>
+            <li>
+              <span className="item-name">Skol Beats</span>
+              <span className="item-price">R$10,00</span>
+            </li>
+            <li>
+              <span className="item-name">Schweppers Drink</span>
+              <span className="item-price">R$8,00</span>
+            </li>
+            <li>
+              <span className="item-name">Red Bull</span>
+              <span className="item-price">R$10,00</span>
+            </li>
+            <li>
+              <span className="item-name">Red Bull Sabores</span>
               <span className="item-price">R$10,00</span>
             </li>
           </ul>
+          <div className="subsection">
+            <h3 className="subsection-title">ÁGUAS</h3>
+            <ul>
+              <li>
+                <span className="item-name">Água Sem Gás</span>
+                <span className="item-price">R$2,00</span>
+              </li>
+              <li>
+                <span className="item-name">Água com Gás</span>
+                <span className="item-price">R$3,50</span>
+              </li>
+              <li>
+                <span className="item-name">Água de Coco</span>
+                <span className="item-price">R$3,00</span>
+              </li>
+              <li>
+                <span className="item-name">Água de Coco Congelada</span>
+                <span className="item-price">R$3,50</span>
+              </li>
+              <li>
+                <span className="item-name">Água de Coco de Sabor</span>
+                <span className="item-price">R$4,00</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       {showScrollToTop && (
